b7d2f775442bb973e67d9f2d8a9cec08002d8e93
Jean Carlos <jeancc.costa@gmail.com>
2025-03-30 20:05:24 -0300

    finalizado a implementação do trabalho

--- src/main/java/com/test/trabalho/jc/Main.java
+++ src/main/java/com/test/trabalho/jc/Main.java
@@ -1,7 +1,31 @@
 package com.test.trabalho.jc;
 
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import com.test.trabalho.jc.entidade.Aluno;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.webkit()
+                    .launch(new BrowserType.LaunchOptions().setHeadless(false));
+            Page page = browser.newPage();
+            page.navigate("https://pt.anotepad.com/");
+            System.out.println(page.title());
+
+            page.getByPlaceholder("Título da Nota").fill("Entrega trabalho TEST DAS 2024");
+            page.getByPlaceholder("Conteúdo da Nota").fill("Nome: Jean\nMatrícula: 202400184590");
+
+            browser.close();
+
+            System.out.println("Automação finalizada com sucesso!");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
+
--- src/test/java/com/test/trabalho/jc/MainTest.java
+++ src/test/java/com/test/trabalho/jc/MainTest.java
@@ -1,7 +1,39 @@
 package com.test.trabalho.jc;
 
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
+
+    @Test
+    public void testMain() {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.webkit().launch(new BrowserType.LaunchOptions().setHeadless(false));
+
+            Page page = browser.newPage();
+            page.navigate("https://pt.anotepad.com");
+
+            String tituloNota = "Entrega trabalho TEST DAS 2024";
+            page.getByPlaceholder("Título da Nota").fill(tituloNota);
+
+            String conteudoNota = "Nome: Jean\nMatrícula: 202400184590";
+            page.getByPlaceholder("Conteúdo da Nota").fill(conteudoNota);
+
+            String tituloReal = page.getByPlaceholder("Título da Nota").inputValue();
+            String corpoReal = page.getByPlaceholder("Conteúdo da Nota").inputValue();
+
+            assertEquals(tituloNota, tituloReal);
+            assertEquals(conteudoNota, corpoReal);
+
+            browser.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
